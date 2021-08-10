bae6f778d99280aefb1110e926a54998da29d35c
Cypress Frankenfeld <iamcypress@gmail.com>
2021-08-10 16:23:12 -0400

    Hello world

---
+++ src/features/game/Game.tsx
@@ -0,0 +1,3 @@
+export function Game() {
+    return <p>hello world</p>
+}
---
+++ src/features/game/gameSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface Position {
+    x: number;
+    y: number;
+}
+
+export interface Unit {
+    position: Position;
+};
+
+export interface GameState {
+    unit: Unit;
+};
+
+const initialState: GameState = {
+    unit: { position: { x: 0, y: 0 } },
+};
+
+export const gameSlice = createSlice({
+    name: 'game',
+    initialState,
+    reducers: {
+        moveUnit: (state: GameState, action: PayloadAction<Position>) => {
+            state.unit.position = action.payload;
+        },
+    },
+});
+
+export default gameSlice.reducer;
